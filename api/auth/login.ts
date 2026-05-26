2b1e6fc29c5a00e7ec7093f8a22c7238e378fac7
n-scovell <N8scovell@gmail.com>
2026-05-26 09:25:34 -0400

    updated login
    login updated

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -1,68 +1,82 @@
-import { userService } from '../../services/userService'
-import { setCorsHeaders } from '../_cors'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+// import { userService } from '../../services/userService'
+// import { setCorsHeaders } from '../_cors'
+// import bcrypt from 'bcryptjs'
+// import jwt from 'jsonwebtoken'
 
-const JWT_SECRET = process.env.JWT_SECRET!
+// const JWT_SECRET = process.env.JWT_SECRET!
+
+// export default async function handler(req: any, res: any) {
+//   setCorsHeaders(res)
+  
+//   // Handle preflight request
+//   if (req.method === 'OPTIONS') {
+//     return res.status(200).end()
+//   }
+//   if (req.method !== 'POST') {
+//     return res.status(405).json({
+//       message: 'Method not allowed',
+//     })
+//   }
+
+//   try {
+//     const { email, password } = req.body
+
+//     const user = await userService.getUserByEmail(email)
+
+//     if (!user) {
+//       return res.status(401).json({
+//         error: 'Invalid credentials',
+//       })
+//     }
+
+//     const validPassword = await bcrypt.compare(
+//       password,
+//       user.password
+//     )
+
+//     if (!validPassword) {
+//       return res.status(401).json({
+//         error: 'Invalid credentials',
+//       })
+//     }
+
+//     const token = jwt.sign(
+//       {
+//         userId: user.id,
+//         email: user.email,
+//       },
+//       JWT_SECRET,
+//       {
+//         expiresIn: '7d',
+//       }
+//     )
+//     return res.status(200).json({
+//       token,
+//       user: {
+//         id: user.id,
+//         email: user.email,
+//         name: user.name,
+//         sport: user.sport,
+//         team: user.team
+//       }
+//     })
+//   } catch (err) {
+//     return res.status(500).json({
+//       error: 'Login failed',
+//     })
+//   }
+// }
+
+import { setCorsHeaders } from '../_cors'
 
 export default async function handler(req: any, res: any) {
   setCorsHeaders(res)
 
-  // Handle preflight request
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
   }
-  if (req.method !== 'POST') {
-    return res.status(405).json({
-      message: 'Method not allowed',
-    })
-  }
 
-  try {
-    const { email, password } = req.body
-
-    const user = await userService.getUserByEmail(email)
-
-    if (!user) {
-      return res.status(401).json({
-        error: 'Invalid credentials',
-      })
-    }
-
-    const validPassword = await bcrypt.compare(
-      password,
-      user.password
-    )
-
-    if (!validPassword) {
-      return res.status(401).json({
-        error: 'Invalid credentials',
-      })
-    }
-
-    const token = jwt.sign(
-      {
-        userId: user.id,
-        email: user.email,
-      },
-      JWT_SECRET,
-      {
-        expiresIn: '7d',
-      }
-    )
-    return res.status(200).json({
-      token,
-      user: {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        sport: user.sport,
-        team: user.team
-      }
-    })
-  } catch (err) {
-    return res.status(500).json({
-      error: 'Login failed',
-    })
-  }
+  return res.status(200).json({
+    message: 'login route works'
+  })
 }
