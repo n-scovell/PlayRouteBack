02e3d382d24840f7e7fb822977acd4c295edb2d5
n-scovell <N8scovell@gmail.com>
2026-05-27 17:45:08 -0400

    Updated email
    email

--- api/auth/request-verification.ts
+++ api/auth/request-verification.ts
@@ -58,7 +58,7 @@ export default async function handler(req: any, res: any) {
     const resend = new Resend(process.env.RESEND_API_KEY)
 
     await resend.emails.send({
-      from: "PlayRoutes <onboarding@resend.dev>",
+      from: "Nathan Scovell <N8scovell@gmail.com>",
       to: email,
       subject: "Your verification code",
       html: `
