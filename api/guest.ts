ff155c2a32477d076467fb50a302fb3d14b176c1
n-scovell <N8scovell@gmail.com>
2026-08-08 09:53:36 -0400

    up
    up

--- api/guest.ts
+++ api/guest.ts
@@ -1,27 +1,21 @@
 import { guestService } from '../services/guestService'
 import { setCorsHeaders } from './_cors'
+
 export default async function handler(req: any, res: any) {
   setCorsHeaders(res)
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
   }
-  
   if (req.method === 'POST') {
     try {
-      const body = typeof req.body === 'string'
-        ? JSON.parse(req.body)
-        : req.body
-
-      const guest = await guestService.createGuest(body)
-
+      const guest = await guestService.createGuest(req.body)
       return res.status(201).json(guest)
     } catch (err) {
       console.error('CREATE GUEST ERROR:', err)
-
+      // return res.status(500).json({ error: err })
       return res.status(500).json({
         error: err instanceof Error ? err.message : err
       })
     }
   }
-
 }
