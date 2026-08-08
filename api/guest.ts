e1ecad281754005231ab426d5d39e026e97651eb
n-scovell <N8scovell@gmail.com>
2026-08-08 09:46:55 -0400

    up
    up

--- api/guest.ts
+++ api/guest.ts
@@ -1,56 +1,27 @@
 import { guestService } from '../services/guestService'
 import { setCorsHeaders } from './_cors'
-
 export default async function handler(req: any, res: any) {
   setCorsHeaders(res)
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
   }
-
-
-
-  // if (req.method === "GET") {
-  //   const badges = await badgeService.getAllBadges()
-  //   return res.status(200).json(badges)
-  // }
-
+  
   if (req.method === 'POST') {
     try {
-      const guest = await guestService.createGuest(req.body)
+      const body = typeof req.body === 'string'
+        ? JSON.parse(req.body)
+        : req.body
+
+      const guest = await guestService.createGuest(body)
+
       return res.status(201).json(guest)
     } catch (err) {
       console.error('CREATE GUEST ERROR:', err)
-      // return res.status(500).json({ error: err })
+
       return res.status(500).json({
         error: err instanceof Error ? err.message : err
       })
-      
     }
   }
-  // if (req.method === 'DELETE') {
-  //   try {
-  //     const { id } = req.body
-  //     await badgeService.deleteBadge(id)
-  //     return res.status(200).json({
-  //       message: 'Badge deleted'
-  //     })
-  //   } catch (err) {
-  //     console.error(err)
-  //     return res.status(500).json({
-  //       error: err instanceof Error ? err.message : err
-  //     })
-  //   }
-  // }
-  // if (req.method === 'PATCH') {
-  //   try {
-  //     const { id, ...data } = req.body
-  //     const badge = await badgeService.updateBadge(id, data)
-  //     return res.status(200).json(badge)
-  //   } catch (err) {
-  //     console.error(err)
-  //     return res.status(500).json({
-  //       error: err instanceof Error ? err.message : err
-  //     })
-  //   }
-  // }
+
 }
--- services/guestService.ts
+++ services/guestService.ts
@@ -16,40 +16,4 @@ export const guestService = {
       }
     })
   }
-
-  // async updateUser(
-  //   id: string,
-  //   data: {
-  //     password?: string
-  //     name?: string
-  //     sport?: string
-  //     team?: string
-  //   }) {
-  //     const updateData = { ...data }
-
-  //     if (updateData.password) {
-  //       updateData.password = await bcrypt.hash(updateData.password, 10)
-  //     }
-
-  //     return prisma.user.update({
-  //       where: { id },
-  //       data: updateData,
-  //     })
-  // },
-
-  // async getUsers() {
-  //   return prisma.user.findMany()
-  // },
-
-  // async getUserById(id: string) {
-  //   return prisma.user.findUnique({
-  //     where: { id },
-  //   })
-  // },
-
-  // async getUserByEmail(email: string) {
-  //   return prisma.user.findUnique({
-  //     where: { email },
-  //   })
-  // },
 }
