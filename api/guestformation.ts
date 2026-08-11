ffbe804f519910db41abff8fd5c4d8151f183772
n-scovell <N8scovell@gmail.com>
2026-08-11 13:00:19 -0400

    Updated
    Up

--- api/guestformation.ts
+++ api/guestformation.ts
@@ -12,6 +12,7 @@ export default async function handler(req: any, res: any) {
     return res.status(200).json(plays)
   }
   if (req.method === 'POST') {
+    console.log('working')
     try {
       const play = await guestService.createFormation(req.body)
       return res.status(201).json(play)
