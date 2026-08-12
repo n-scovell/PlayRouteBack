4c17580584723ed3bfcf18a12b5e3f6d3e5b4bd8
n-scovell <N8scovell@gmail.com>
2026-08-11 21:35:20 -0400

    Up
    Figuring out why my method is pending

--- api/guestformation.ts
+++ api/guestformation.ts
@@ -1,39 +1,39 @@
 import { guestFormation } from '../services/guestFormation'
 import { setCorsHeaders } from './_cors'
 
-export default function handler(req: any, res: any) {
+export default async function handler(req: any, res: any) {
   setCorsHeaders(res)
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
   }
 
-  return res.status(200).json({
-    hello: 'guestformation'
+  if (req.method === 'GET') {
+    const { guestId } = req.query
+    const formations = await guestFormation.getGuestFormations(guestId)
+    return res.status(200).json(formations)
+  }
+
+  if (req.method === 'POST') {
+    try {
+      const formation = await guestFormation.createFormation(req.body)
+      return res.status(201).json(formation)
+    } catch (err: any) {
+      if (err.message === 'Guest formation limit reached') {
+        return res.status(403).json({
+          error: 'Guest formation limit reached'
+        })
+      }
+
+      console.error('GUEST FORMATION ERROR:', err)
+
+      return res.status(500).json({
+        error: err.message || 'Failed to create guest formation'
+      })
+    }
+  }
+
+  return res.status(405).json({
+    error: 'Method not allowed'
   })
 }
-
-// export default async function handler(req: any, res: any) {
-//   setCorsHeaders(res)
-//   if (req.method === 'GET') {
-//     const { guestId } = req.query
-//     const plays = await guestFormation.getGuestFormations(guestId)
-//     return res.status(200).json(plays)
-//   }
-//   if (req.method === 'POST') {
-//     try {
-//       const play = await guestFormation.createFormation(req.body)
-//       return res.status(201).json(play)
-//     } catch (err: any) {
-//       if (err.message === 'Guest formation limit reached') {
-//         return res.status(403).json({
-//           error: 'Guest formation limit reached'
-//         })
-//       }
-//       return res.status(500).json({
-//         error: err
-//       })
-//     }
-//   }
-// }
-
