3f8456083016b4bfd2f7ba5fb53bf3273f4730b9
n-scovell <N8scovell@gmail.com>
2026-08-26 12:16:53 -0400

    Updated
    Updated

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -59,6 +59,8 @@ export default async function handler(req: any, res: any) {
         name: user.name,
         sport: user.sport,
         team: user.team,
+        plan: user.plan,
+        subscriptionStatus: user.subscriptionStatus,
       },
     })
   } catch (err) {
